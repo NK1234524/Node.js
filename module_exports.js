049a475d342517466cd1ed05d1d3464e7f7ed86a
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-01 22:16:33 +0530

    Update

---
+++ module_exports.js
@@ -0,0 +1,15 @@
+const pie = 3.14;
+const sum = (a,b)=>a+b;
+const mul = (a,b)=>a*b;
+const div = (a,b)=>a/b;
+
+let math={
+    sum:sum,
+    mul:mul,
+    pie:pie,
+    div:div
+}
+module.exports=math;
+
+//for individual 
+module.exports.g =9.8;
