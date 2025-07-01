32df2bf1a515111d9e6eb30a9e05a88b20500704
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-01 22:15:44 +0530

    Update

--- Node.js
+++ Node.js
@@ -1,5 +1,11 @@
-let number =  34;
-for (let i=0 ; i<number ; i++){
-    console.log("Hello world");
-}
-console.log("Bye everyone");
+// let number =  34;
+// for (let i=0 ; i<number ; i++){
+//     console.log("Hello world");
+// }
+// console.log("Bye everyone");
+
+let operation = require("./module_exports");
+console.log(operation.sum(2,3));
+console.log(operation.mul(2,3));
+console.log(operation.div(2,3));
+console.log(operation.g);
